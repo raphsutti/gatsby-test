e22f91666c8ef735902afb198215ff4b0c1697d7
Raph <rsuttiyotin@seek.com.au>
2021-10-03 08:33:56 +1100

    Add Home and About pages with shared Layout comp

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi! I created this site using Gatsby</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
